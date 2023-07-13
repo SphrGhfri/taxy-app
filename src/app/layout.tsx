66625743351b7525a680bc87c83d48404daeb758
Mehdi Nourollah <mehdi.nourollah@axxessio.com>
2023-07-13 18:16:11 +0200

    theme added dark mode only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 // app/layout.js - no directives needed
 import '@/app/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { darkTheme } from './theme/themes'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body id="__next">{children}</body>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline>
+          <body id="__next">{children}</body>
+        </CssBaseline>
+      </ThemeProvider>
     </html>
   )
 }
