d379fc3d19450d61ac90e2e646127575966e6fca
Mehdi Nourollah <mehdi.nourollah@axxessio.com>
2023-07-14 16:55:45 +0200

    active path marked

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,10 @@ import { TItemType } from '@/lib/menu-items';
 import { MenuItems } from '@/lib/menu-items'
 
 import { Collapse, Drawer, List, ListItemButton, ListItemText } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSelectedLayoutSegments } from 'next/navigation';
 
 const drawerWidth = 240;
 
@@ -42,6 +42,7 @@ const DrawerMenuItem = ({ item, indent }:
     { item: TItemType, indent?: boolean }) => {
     const [open, setOpen] = useState<boolean>(false);
     const router = useRouter();
+    const segment = useSelectedLayoutSegments();
 
     const handleClick = () => {
         if (item.path)
@@ -51,6 +52,9 @@ const DrawerMenuItem = ({ item, indent }:
     const collapseToggle = () => {
         setOpen(!open);
     }
+
+    const isActive = '/' + segment?.join('/') === item.path;
+
 
     if (item.items) {
         return <><ListItemButton sx={{ pl: 4 }}>
@@ -67,7 +71,7 @@ const DrawerMenuItem = ({ item, indent }:
         </>
     }
     else {
-        return <ListItemButton sx={{ pl: indent ? 6 : 4 }} onClick={handleClick}>
+        return <ListItemButton selected={isActive} sx={{ pl: indent ? 6 : 4 }} onClick={handleClick}>
             <ListItemText primary={item.name} />
         </ListItemButton>
 
